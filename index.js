d8b90058b445fe04b295aa0bca2740c60eb0e082
Matias Paavilainen <matias.paavilainen@gmail.com>
2024-01-09 13:45:58 +0200

    3.6 tehty

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    {
+        id:1,
+        name: "Arto Hellas",
+        number: "040-123456"
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345"
+    },
+    {
+        id: 4,
+        name: "Mary Poppendick",
+        number: "39-23-6423122"
+    },
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    
+    response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date(Date.now()).toString()}</p>
+        `)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+const generateId = () => {
+    let rand = Math.floor(Math.random() * 1000)
+    if (persons.map(p => p.id).includes(rand)) {
+        rand = Math.floor(Math.random() * 1000)
+    }
+    return rand
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'No name or number'
+        })
+    }
+
+    if (persons.map(p => p.name).includes(body.name)) {
+        return response.status(400).json({
+            error: 'Name already exists'
+        })
+    }
+
+    
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number,
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
