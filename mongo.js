6b4b8055c5ab1e60d212d214c434b36d089714cb
Matias Paavilainen <101323404+matiaspaavilainen@users.noreply.github.com>
2024-05-07 11:36:51 +0300

    3.12 tehty

--- mongo.js
+++ mongo.js
@@ -25,10 +25,10 @@ if (process.argv.length == 3) {
         .then(result => {
             console.log('Phonebook:')
             result.forEach(person => {
-                console.log(person)
+                console.log(person.name, person.number)
             });
+            mongoose.connection.close()
         })
-    mongoose.connection.close()
 
 } else {
     const person = new Person({
