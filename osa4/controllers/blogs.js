9a0547405753642e6cc023ff844c337ffd00aadf
Matias Paavilainen <101323404+matiaspaavilainen@users.noreply.github.com>
2024-06-10 12:31:03 +0300

    fixed typo in put

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -31,9 +31,10 @@ blogsRouter.put('/:id', async (request, response) => {
     const body = request.body
     const blog = {
         title: body.title,
-        author: body.title,
+        author: body.author,
         url: body.url,
         likes: body.likes,
+        user: body.user,
     }
 
     const result = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
